1e7f0faac204c4c1e0c01e910fc157855d9c9f0b
Alex Goodwin <goodwinalexjr@knights.ucf.edu>
2021-06-01 16:31:23 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //any username for the first test
+        System.out.print("What is the Username? ");
+        String userName = sc.nextLine();
+        //password is abc$123
+        System.out.print("What is the password? ");
+        String password = sc.nextLine();
+        if (password.equals("abc$123")){
+            System.out.println("Welcome!");
+        }
+        else{
+            System.out.println("I dont know you.");
+        }
+    }
+}
